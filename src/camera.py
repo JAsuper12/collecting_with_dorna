a0c23c06578fbfb47312de9ab5241b305f29a59a
lennarttoenjess <38102908+lennarttoenjess@users.noreply.github.com>
2020-01-21 18:00:04 +0100

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -27,7 +27,7 @@ class Camera:
 
         # Variabeln zur Farberkennung
         blue_lower = [51, 0, 0]
-        blue_upper = [255, 51, 51]
+        blue_upper = [255, 62, 62]
         self.boundaries = [(blue_lower, blue_upper)]
         self.cX = None
         self.cY = None
@@ -35,10 +35,11 @@ class Camera:
         # Variablen zur Positionierung
         self.ball_position = []
         self.qr_centres = []
-        self.world_points_array = []
-        self.localization_qr_codes = []
+        self.world_points = []
         self.qr_codes = None
-        self.image_points = None
+        self.image_points_of_qr_codes = None
+        self.localization_qr_codes = []
+        self.world_localization_qr_codes = []
 
     def show_image(self):
         nRet = ueye.is_InitCamera(self.h_cam, None)
@@ -55,6 +56,7 @@ class Camera:
         nRet = ueye.is_CaptureVideo(self.h_cam, ueye.IS_DONT_WAIT)
         nRet = ueye.is_InquireImageMem(self.h_cam, self.pcImageMemory, self.MemID, self.width, self.height,
                                        self.nBitsPerPixel, self.pitch)
+
 
         while nRet == ueye.IS_SUCCESS:
             array = ueye.get_data(self.pcImageMemory, self.width, self.height, self.nBitsPerPixel, self.pitch, copy=False)
@@ -127,7 +129,7 @@ class Camera:
                 break
             circularity = 4 * m.pi * (area / (perimeter * perimeter))
             # print(circularity)
-            if 0.7 < circularity < 1.5:
+            if 0.25 < circularity < 1.5:
                 contours_circles.append(con)
 
         for cnt in contours_circles:
@@ -142,8 +144,9 @@ class Camera:
 
     def qr_decoder(self):
         self.qr_centres.clear()
-        self.world_points_array.clear()
+        self.world_points.clear()
         self.localization_qr_codes.clear()
+        self.world_localization_qr_codes.clear()
         self.qr_codes = pyzbar.decode(self.dst)
         # loop over the detected barcodes
         for qr in self.qr_codes:
@@ -165,43 +168,64 @@ class Camera:
                 world_point = (70, 10, 0)
                 if len(self.localization_qr_codes) < 3:
                     self.localization_qr_codes.append(centre)
+                    self.world_localization_qr_codes.append([70, 10, 0])
             elif data == "(60, 40, 0)":
                 world_point = (60, 40, 0)
                 if len(self.localization_qr_codes) < 3:
                     self.localization_qr_codes.append(centre)
+                    self.world_localization_qr_codes.append([60, 40, 0])
             elif data == "(40, 60, 0)":
                 world_point = (40, 60, 0)
                 if len(self.localization_qr_codes) < 3:
                     self.localization_qr_codes.append(centre)
+                    self.world_localization_qr_codes.append([40, 60, 0])
             elif data == "(-40, 60, 0)":
                 world_point = (-40, 60, 0)
                 if len(self.localization_qr_codes) < 3:
                     self.localization_qr_codes.append(centre)
+                    self.world_localization_qr_codes.append([-40, 60, 0])
             elif data == "(-60, 40, 0)":
                 world_point = (-60, 40, 0)
                 if len(self.localization_qr_codes) < 3:
                     self.localization_qr_codes.append(centre)
+                    self.world_localization_qr_codes.append([-60, 40, 0])
             elif data == "(-70, 10, 0)":
                 world_point = (-70, 10, 0)
                 if len(self.localization_qr_codes) < 3:
                     self.localization_qr_codes.append(centre)
+                    self.world_localization_qr_codes.append([-70, 10, 0])
 
-            self.world_points_array.append(world_point)
+            self.world_points.append(world_point)
+            world_points = np.array(self.world_points, dtype="float")
+
             # draw the barcode data and barcode type on the image
             text = "{}".format(data)
             cv2.putText(self.dst, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-        # print(self.world_points_array)
+            # prüfen, ob die qr Codes gut verteilt sind
+            positive_qr_codes = 0
+            negative_qr_codes = 0
+            for coordinates in world_points:
+                if coordinates[0] > 0:
+                    positive_qr_codes = positive_qr_codes + 1
+                else:
+                    negative_qr_codes = negative_qr_codes + 1
+
+            if positive_qr_codes >= 2 and negative_qr_codes >= 2:
+                valid_qr_spread = True
+            else:
+                valid_qr_spread = False
+
+        # print(self.world_points)
         # print(self.localization_qr_codes)
 
-        if len(self.qr_centres) >= 4:
-            # print(world_points_array)
-            self.image_points = np.array(self.qr_centres, dtype="float")
-            world_points = np.array(self.world_points_array, dtype="float")
+        if len(self.qr_centres) >= 4 and valid_qr_spread:
             # print(world_points)
-            # print(image_points)
+            self.image_points_of_qr_codes = np.array(self.qr_centres, dtype="float")
+            # print(world_points)
+            # print(self.image_points_of_qr_codes)
 
-            _, rvecs, tvecs, = cv2.solvePnP(world_points, self.image_points, self.camera_matrix, self.dist_coeff)
+            _, rvecs, tvecs = cv2.solvePnP(world_points, self.image_points_of_qr_codes, self.camera_matrix, self.dist_coeff)
 
             origin, jacobian = cv2.projectPoints(np.array([(0.0, 0.0, 0.0)]), rvecs, tvecs, self.camera_matrix, self.dist_coeff)
             z_axis, jacobian = cv2.projectPoints(np.array([(0.0, 0.0, 10.0)]), rvecs, tvecs, self.camera_matrix, self.dist_coeff)
@@ -227,23 +251,58 @@ class Camera:
     def get_ball_position(self):
         if len(self.localization_qr_codes) == 3:
             world_distance_of_qr_codes = []
-            for i in range(len(self.world_points_array)):
-                x_1, y_1, z_1 = self.world_points_array[i]
-                if i < len(self.world_points_array) - 1:
+            for i in range(len(self.world_points)):
+                x_1, y_1, z_1 = self.world_points[i]
+                if i < len(self.world_points) - 1:
                     j = i + 1
                 else:
                     break
-                while j <= len(self.world_points_array):
-                    print(j)
-                    x_2, y_2, z_2 = self.world_points_array[j]
+                while j <= len(self.world_points):
+                    # print(j)
+                    x_2, y_2, z_2 = self.world_points[j]
                     world_distance_of_qr_codes.append(m.sqrt((m.pow((x_1 - x_2), 2) + m.pow((y_1 - y_2), 2))))
-                    if j < len(self.world_points_array) - 1:
+                    if j < len(self.world_points) - 1:
                         j = j + 1
                     else:
                         break
-            print(world_distance_of_qr_codes)
+            #print(world_distance_of_qr_codes)
+
+            sum = 0
+            for x in world_distance_of_qr_codes:
+                sum = sum + x
+            average_world_distance_of_qr_codes = sum / len(world_distance_of_qr_codes)
+            # print(average_world_distance_of_qr_codes)
+
+            image_distance_of_qr_codes = []
+            for i in range(len(self.image_points_of_qr_codes)):
+                u_1 = self.image_points_of_qr_codes[i][0]
+                v_1 = self.image_points_of_qr_codes[i][1]
+                if i < len(self.world_points) - 1:
+                    j = i + 1
+                else:
+                    break
+                while j <= len(self.image_points_of_qr_codes):
+                    u_2 = self.image_points_of_qr_codes[j][0]
+                    v_2 = self.image_points_of_qr_codes[j][1]
+                    image_distance_of_qr_codes.append(m.sqrt((m.pow((u_1 - u_2), 2) + m.pow((v_1 - v_2), 2))))
+                    if j < len(self.world_points) - 1:
+                        j = j + 1
+                    else:
+                        break
+            # print(image_distance_of_qr_codes)
+
+            sum = 0
+            for x in image_distance_of_qr_codes:
+                sum = sum + x
+            average_image_distance_of_qr_codes = sum / len(image_distance_of_qr_codes)
+            # print(average_image_distance_of_qr_codes)
+
+            cm_per_pixel = average_world_distance_of_qr_codes / average_image_distance_of_qr_codes
+            # print(cm_per_pixel)
 
             distance_balls_to_qr_code = []
+
+            # print(len(self.ball_position))
             i = 0
             for (ball_x, ball_y) in self.ball_position:
                 distance = []
@@ -258,11 +317,52 @@ class Camera:
                         cv2.circle(self.dst, (qr_x, qr_y), int(distance[n_distance]), (0, 0, 255), 2)
                     n_distance = n_distance + 1
                 distance_balls_to_qr_code.append(distance)
-                # print(distance_of_balls)
                 i = i + 1
-            self.localization_qr_codes = np.array(self.localization_qr_codes)
-            real_distance = m.sqrt(m.pow(self.localization_qr_codes[1][0] - self.localization_qr_codes[2][0], 2) + m.pow(self.localization_qr_codes[1][1] - self.localization_qr_codes[2][1], 2))
-            # print(real_distance)
+            # print(distance_balls_to_qr_code)
+
+            # print(self.world_localization_qr_codes)
+
+            equations = []
+
+            '''for j in range(len(self.world_localization_qr_codes)):
+                x = 1
+                x_k = self.world_localization_qr_codes[j][0]
+                y = 1
+                y_k = self.world_localization_qr_codes[j][1]
+                k = (distance_balls_to_qr_code[0][j] * cm_per_pixel) ** 2
+                equations.append([x, x_k, y, y_k, k])
+            print(equations)'''
+
+            a_1 = 30
+            b_1 = 30
+            k_1 = 22.32
+            a_2 = 0
+            b_2 = 40
+            k_2 = 19.41
+
+            a = a_1 - a_2
+            print(a)
+            k = k_2 - k_1
+            print(k)
+            b = b_1 - b_2
+            print(b ** 2)
+            c = (k ** 2 + b ** 2 - a ** 2) / (2 * a)
+            print(c)
+            d = -1 - (b / a) ** 2
+            print(d)
+            e = 2 * b_1 + 2 * b * c / a
+            print(e)
+            f = c ** 2 + b_1 ** 2 - k_1 ** 2
+            print(f)
+
+            p = e / d
+            print(p)
+            q = -f / d
+            print(q)
+
+            y_1 = -p / 2 + m.sqrt((p / 2) ** 2 - q)
+            y_2 = -p / 2 - m.sqrt((p / 2) ** 2 - q)
+            print(y_1, y_2)
 
 
 if __name__ == "__main__":
